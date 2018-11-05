29b5d3fe1292c157252c3d0245b4329de269efb6
Shriya Gayatri <35976311+shriyagayatri@users.noreply.github.com>
2018-11-05 20:49:51 +0530

    In C

--- String Permutation/code.c
+++ String Permutation/code.c
@@ -1,1 +1,40 @@
+#include <stdio.h> 
+#include <string.h> 
+
+void swap(char *x, char *y) 
+{ 
+	char temp; 
+	temp = *x; 
+	*x = *y; 
+	*y = temp; 
+} 
+
+/* Function to print permutations of string 
+This function takes three parameters: 
+1. String 
+2. Starting index of the string 
+3. Ending index of the string. */
+void permute(char *a, int s, int l) 
+{ 
+int i; 
+if (s == l) 
+	printf("%s\n", a); 
+else
+{ 
+	for (i = s; i <= l; i++) 
+	{ 
+		swap((a+s), (a+i)); 
+		permute(a, s+1, l); 
+		swap((a+s), (a+i)); //backtrack 
+	} 
+} 
+} 
+
+int main() 
+{ 
+	char str[] = "ABCDEF"; 
+	int n = strlen(str); 
+	permute(str, 0, n-1); 
+	return 0; 
+} 
 
