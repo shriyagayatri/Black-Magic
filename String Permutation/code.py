dc98fc65bb78dffc2305c66b70f1f8c80f0295b6
Shriya Gayatri <35976311+shriyagayatri@users.noreply.github.com>
2018-11-05 21:04:17 +0530

    Update code.py

--- String Permutation/code.py
+++ String Permutation/code.py
@@ -1,1 +1,19 @@
 
+
+def toString(List): 
+	return ''.join(List) 
+
+def permute(a, s, l): 
+	if s==l: 
+		print toString(a) 
+	else: 
+		for i in xrange(s,l+1): 
+			a[s], a[i] = a[i], a[s] 
+			permute(a, s+1, l) 
+			a[s], a[i] = a[i], a[s] # backtrack 
+
+string = "ABC"
+n = len(string) 
+a = list(string) 
+permute(a, 0, n-1) 
+
